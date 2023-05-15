f248d74596e6e6ff5cc9c37ed057b232910c2f53
OriqqUack <jkp8277@naver.com>
2023-05-16 00:21:21 +0900

    0516

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -1,19 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    [SerializeField]
-    TextMeshProUGUI _text;
+    
+
+    enum Buttons
+    {
+        PointButton
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText
+    }
+
+    enum Images
+    {
+        itemIcon,
+    }
+
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<TextMeshProUGUI>(typeof(Texts));
+        Bind<Image>(typeof(Images));
+
+        GetButton((int)Buttons.PointButton).gameObject.AddUIEvent(OnButtonClicked);
+
+        GameObject go = GetImage((int)Images.itemIcon).gameObject;
+        AddUIEvent(go, (PointerEventData data) => { go.gameObject.transform.position = data.position; }, Define.UIEvent.Drag);
+       
+    }
+
+    
 
     int _score = 0;
 
-    public void OnButtonClicked()
+    public void OnButtonClicked(PointerEventData data)
     {
         _score++;
-        _text.text = $"점수 : {_score}";
+        GetText((int)Texts.ScoreText).text = $"점수 : {_score}";
     }
 }
